15378add42a798699f18caf0279b628a51488028
mrfyock <117535336+mrfyock@users.noreply.github.com>
2023-04-02 21:51:41 -0400

    Finalized code.
    Fixed darkmode light mode. Fixed index.html dynamics, fixed pages to respond dynamically to size changes. added functions to view selected galleries.

---
+++ Pixifyfinal/scripts/script.js
@@ -0,0 +1,169 @@
+const imageSets = [
+    {
+      id:1,  
+      name: 'Andrew Smith',
+      title: 'Innocence',
+      images: [
+        'https://images.unsplash.com/photo-1512540315028-2c1a6497da04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1543332143-4e8c27e3256f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1734&q=80',
+        'https://images.unsplash.com/photo-1612209246511-5b81949e5fdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+        'https://images.unsplash.com/photo-1492680967812-4b9226a079de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80'
+      ],
+    },
+    {
+      id:2,
+      name: 'Jane Doe',
+      title: 'Nature Adventures',
+      images: [
+        'https://images.unsplash.com/photo-1533692328991-08159ff19fca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80',
+        'https://images.unsplash.com/photo-1508515803898-7bb2d7703093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1503557122744-b650066ba62f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1516298773066-c48f8e9bd92b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1598726668148-99946ef1cb42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1732&q=80'
+      ],
+    },
+    {
+      id:3,
+      name: 'Irna Tawsen',
+      title: 'Faces of the World',
+      images: [
+        'https://images.unsplash.com/photo-1674574124649-778f9afc0e9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1532635241-17e820acc59f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1630&q=80',
+        'https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1509099836639-18ba1795216d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80',
+        'https://images.unsplash.com/photo-1604186837056-8e7c2867b6f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+      ],
+    },
+    {
+      id:4,
+      name: 'Mark Dale',
+      title: 'Wild Animals',
+      images: [
+        'https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80',
+        'https://images.unsplash.com/photo-1504173010664-32509aeebb62?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1654&q=80',
+        'https://images.unsplash.com/photo-1557050543-4d5f4e07ef46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
+        'https://images.unsplash.com/photo-1531494391841-6ac2ef3859b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1550358864-518f202c02ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+      ]
+    },
+    {
+      id:5,
+      name: 'Jane Doe',
+      title: 'Nature Adventures',
+      images: [
+        'https://images.unsplash.com/photo-1533692328991-08159ff19fca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80',
+        'https://images.unsplash.com/photo-1508515803898-7bb2d7703093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1503557122744-b650066ba62f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1516298773066-c48f8e9bd92b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1598726668148-99946ef1cb42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1732&q=80'
+      ],
+    },
+    
+  ];
+
+//Create the gallery of submissions
+const displayGallery = (list) => {
+  const submissionsContainer = document.getElementById("submissions-container");
+  const allSubmissions = list.map(item => {
+    return `<div class="submission">
+              <img src="${item.images}" alt="${item.title}" class="submission-image"/>
+                <div class="gallery-info">              
+                  <div>
+                    <p class="submission-info">${item.title} - ${item.name}</p>
+                  </div>
+                  <div>
+                      <a href= "details.html?id=${item.id}"><button class="view-gallery-btn">See More</button></a>
+                  </div>
+                </div>     
+            </div>`;
+  })
+  console.log(allSubmissions);
+  submissionsContainer.innerHTML = allSubmissions.join("");
+  
+}
+//load images function
+  window.onload = function() {
+  displayGallery(imageSets);
+}
+
+// create darkmode / light mode functionality
+
+const darkmodeBtn = document.querySelector(".darkmode");
+
+//darkmode style//
+darkmodeBtn.style.background="none";
+darkmodeBtn.style.border="none";
+darkmodeBtn.style.color= "#7BFFE9";
+let anchors = document.getElementsByClassName("submission-info");
+
+
+//darkmode function//
+const toggleMode = () => {
+ const body = document.querySelector("body");
+ const submissionHeader = document.querySelector("h2");
+ const submissionText = document.querySelector("#submissions-container");
+ const galleryBtn = document.querySelectorAll(".view-gallery-btn"); 
+ if(body.style.backgroundColor === "white"){
+    body.style.backgroundColor = "#2A3B49";
+    submissionHeader.style.color = "white";
+    darkmodeBtn.innerHTML = "Light Mode";
+    submissionText.style.color = "white";
+    for (let i = 0; i < galleryBtn.length; i++) {
+      galleryBtn[i].style.color = "white";
+    }; 
+ }else{
+    body.style.backgroundColor = "white";
+    darkmodeBtn.innerHTML = "Dark Mode"; 
+    submissionHeader.style.color = "black";
+    submissionText.style.color = "black";
+    for (let i = 0; i < galleryBtn.length; i++) {
+      galleryBtn[i].style.color = "black";
+ };
+}
+}
+//activate theme
+darkmodeBtn.onclick = toggleMode;
+
+//Submission form gallery
+
+const submitBtn = document.querySelector("#submit-btn");
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  //values
+  const name = document.querySelector('input[name="name"]').value;
+  const title = document.querySelector('input[name="title"]').value;
+  const urlOne = document.querySelector('input[name="url-one"]').value;
+  const urlTwo = document.querySelector('input[name="url-two"]').value;
+  const urlThree = document.querySelector('input[name="url-three"]').value;
+  const urlFour = document.querySelector('input[name="url-four"]').value;
+  const urlFive = document.querySelector('input[name="url-five"]').value;
+  //create new gallery div
+
+  const newSubmission = document.createElement("div");
+  newSubmission.innerHTML = `<div class="submission">
+      <img src="${urlOne}" alt="${title}" class="submission-image"/>
+        <div class="gallery-info">
+          <div>
+            <p class="submission-info">${title} - ${name}</p>
+          </div>
+          <div>
+            <button class="view-gallery-btn">See More</button>
+          </div>
+        </div>
+    </div>`;
+  const submissionsContainer = document.querySelector("#submissions-container");
+  submissionsContainer.insertBefore(newSubmission, submissionsContainer.firstChild);
+  // clear the submission form
+  document.querySelector('input[name="name"]').value = '';
+  document.querySelector('input[name="title"]').value = '';
+  document.querySelector('input[name="url-one"]').value = '';
+  document.querySelector('input[name="url-two"]').value = '';
+  document.querySelector('input[name="url-three"]').value = '';
+  document.querySelector('input[name="url-four"]').value = '';
+  document.querySelector('input[name="url-five"]').value = '';
+
+  
+});
+
---
+++ Pixifyfinal/scripts/submissions-script.js
@@ -0,0 +1,145 @@
+const imageSets = [
+    {
+      id:1,  
+      name: 'Andrew Smith',
+      title: 'Innocence',
+      images: [
+        'https://images.unsplash.com/photo-1512540315028-2c1a6497da04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1543332143-4e8c27e3256f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1734&q=80',
+        'https://images.unsplash.com/photo-1612209246511-5b81949e5fdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+        'https://images.unsplash.com/photo-1492680967812-4b9226a079de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80'
+      ],
+    },
+    {
+      id:2,
+      name: 'Jane Doe',
+      title: 'Nature Adventures',
+      images: [
+        'https://images.unsplash.com/photo-1533692328991-08159ff19fca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80',
+        'https://images.unsplash.com/photo-1508515803898-7bb2d7703093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1503557122744-b650066ba62f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1516298773066-c48f8e9bd92b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1598726668148-99946ef1cb42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1732&q=80'
+      ],
+    },
+    {
+      id:3,
+      name: 'Irna Tawsen',
+      title: 'Faces of the World',
+      images: [
+        'https://images.unsplash.com/photo-1674574124649-778f9afc0e9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1532635241-17e820acc59f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1630&q=80',
+        'https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1509099836639-18ba1795216d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80',
+        'https://images.unsplash.com/photo-1604186837056-8e7c2867b6f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+      ],
+    },
+    {
+      id:4,
+      name: 'Mark Dale',
+      title: 'Wild Animals',
+      images: [
+        'https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80',
+        'https://images.unsplash.com/photo-1504173010664-32509aeebb62?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1654&q=80',
+        'https://images.unsplash.com/photo-1557050543-4d5f4e07ef46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
+        'https://images.unsplash.com/photo-1531494391841-6ac2ef3859b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        'https://images.unsplash.com/photo-1550358864-518f202c02ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+      ]
+    }
+
+  ];
+
+  let currentId;
+  let currentImgIndex = 0;
+
+  window.onload = function(){
+    // grab url parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    currentId = urlParams.get("id");
+    console.log(currentId);
+    console.log("works")
+       
+
+    // get gallery that user selected from all albums
+    const currentGallery = imageSets.filter(item => item.id == currentId);
+    console.log(currentGallery);
+    const gallery = `
+    <div class="image-gallery">
+        <div class="image-title">
+          <h1>${currentGallery[0].title}</h1>
+        </div>
+        <div class="image-name">
+          <h3>${currentGallery[0].name}</h3>
+        </div>
+        
+        <img id="current-img" src=${currentGallery[0].images[0]} alt=${currentGallery[0].name}>
+        
+        <div class="buttons-container">
+          <p class="counter">${currentImgIndex +1} of ${currentGallery[0].images.length}</p>
+          <button class="gallery-button" id="previous">Previous</button>
+          <button class="gallery-button"id="next">Next</button>
+        </div>
+      </div>`
+
+    const galleryContainer = document.querySelector("main");
+    galleryContainer.innerHTML = gallery;
+
+  
+  //next button and previous buttons  
+  const currentImgElement = document.getElementById("current-img");
+  const nextButton = document.getElementById("next");
+  const prevButton = document.getElementById("previous");
+  nextButton.addEventListener("click", () => {
+    currentImgIndex++;
+    if (currentImgIndex >= currentGallery[0].images.length) {
+      currentImgIndex = 0;
+    }
+    //update the counter
+    currentImgElement.src = currentGallery[0].images[currentImgIndex];
+    const counter = document.querySelector(".counter");
+    counter.innerHTML = `${currentImgIndex+1} of ${currentGallery[0].images.length}`;
+  });
+  prevButton.addEventListener("click", () =>{
+    currentImgIndex--;
+    if (currentImgIndex < 0) {
+      currentImgIndex = currentGallery[0].images.length - 1;
+    }
+    currentImgElement.src = currentGallery[0].images[currentImgIndex];
+    const counter = document.querySelector(".counter");
+    counter.innerHTML = `${currentImgIndex+1} of ${currentGallery[0].images.length}`;
+  });
+};
+
+
+//darkmode function//
+const darkmodeBtn = document.querySelector(".darkmode");
+
+const toggleMode = () => {
+  const body = document.querySelector("body");
+  const galleryText = document.querySelector(".image-gallery");
+  const buttons = document.querySelectorAll(".gallery-button")
+ 
+  if(body.style.backgroundColor === "white"){
+    body.style.backgroundColor = "#2A3B49";
+    galleryText.style.color = "white";
+    darkmodeBtn.innerHTML = "Light Mode";
+  
+  }else{
+    body.style.backgroundColor = "white";
+    darkmodeBtn.innerHTML = "Dark Mode"; 
+    galleryText.style.color = "black";
+   
+  };
+ }
+ 
+ //activate theme
+darkmodeBtn.onclick = toggleMode;
+
+//darkmode style
+darkmodeBtn.style.background="none";
+darkmodeBtn.style.border="none";
+darkmodeBtn.style.color= "#7BFFE9";
+
+
+
